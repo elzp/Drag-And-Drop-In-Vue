01df16209d8fd96b2339af01bd1e1d98f8bbba16
elzp <86740659+elzp@users.noreply.github.com>
2022-11-17 19:39:25 +0100

    Change structure of articles' data

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
     <div className="flex justify-center items-top">
       <ArticlesContainer
         :title="title[0]"
-        :articlesData="articlesData.Drafts"
+        :articlesData="articlesData.filter((it) => it.container === title[0])"
       />
       <ArticlesContainer
         :title="title[1]"
-        :articlesData="articlesData.Published"
+        :articlesData="articlesData.filter((it) => it.container === title[1])"
       />
     </div>
   </div>
@@ -24,14 +24,32 @@ export default {
   data() {
     return {
       title: ['Drafts', 'Published'],
-      articlesData: {
-        Drafts: [
-          { name: 'art1', date: '10.11.2022', content: 'art1' },
-          { name: 'art2', date: '11.11.2022', content: 'art2' },
-          { name: 'art3', date: '12.11.2022', content: 'art3' },
-        ],
-        Published: [{ name: 'art4', date: '14.11.2022', content: 'art4' }],
-      },
+      articlesData: [
+        {
+          name: 'art1',
+          date: '10.11.2022',
+          content: 'art1',
+          container: 'Drafts',
+        },
+        {
+          name: 'art2',
+          date: '11.11.2022',
+          content: 'art2',
+          container: 'Drafts',
+        },
+        {
+          name: 'art3',
+          date: '12.11.2022',
+          content: 'art3',
+          container: 'Drafts',
+        },
+        {
+          name: 'art4',
+          date: '14.11.2022',
+          content: 'art4',
+          container: 'Published',
+        },
+      ],
     };
   },
 };
@@ -41,13 +59,5 @@ export default {
 h1,
 p {
   font-family: Lato;
-}
-
-.hide {
-  display: none;
-}
-
-.drag-over {
-  border: dashed 3px red;
 }
 </style>
--- src/components/ArticlesContainer.vue
+++ src/components/ArticlesContainer.vue
@@ -1,18 +1,28 @@
 <template>
   <div
-    className="bg-white overflow-hidden rounded-md p-30 max-w-xs flex-1 flex flex-col m-2"
+    class="
+      bg-white
+      overflow-hidden
+      rounded-md
+      p-30
+      max-w-xs
+      flex-1 flex flex-col
+      m-2
+    "
     draggable="false"
   >
-    <h2 className="text-4xl p-5 text-center" draggable="false">{{ title }}</h2>
+    <h2 class="text-4xl p-5 text-center" draggable="false">{{ title }}</h2>
     <main
-      className="p-10 bg-gray-300 flex-1"
+      class="p-10 bg-gray-300 flex-1"
+      :class="{ 'drag-over': dragover }"
       draggable="false"
       v-for="article in articlesData"
       @dragstart="dragStart($event, article)"
-      @dragenter="dragEnter"
-      @dragover="dragOver"
-      @sragleave="dragLeave"
-      @ondrop="drop"
+      @dragenter.prevent="dragEnter($event)"
+      @dragover.prevent="dragOver"
+      @dragleave="dragLeave($event)"
+      @drop="drop($event)"
+      @dragend="dragEnd"
     >
       <Article
         :name="article.name"
@@ -32,10 +42,15 @@ export default {
     title: String,
     articlesData: Array,
   },
+  data() {
+    return {
+      dragover: false,
+    };
+  },
   methods: {
     dragStart(e, item) {
+      //  e.preventDefault();
       // https://learnvue.co/tutorials/vue-drag-and-drop
-      console.log('start', e.target.id, item.name);
       e.dataTransfer.setData('text', item.name);
     },
   },
